a653050e54ac5d6f07922fc3e80324a3b2c23a3c
liamgen <liamgen.js@proton.me>
2023-09-03 15:14:46 +0200

    1.0.0

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const https = require('https');
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/api', (req, res) => {
-    res.send('hey')
-  })
+app.get('/api/tomacloud/:url', (req, res) => {
+    res.send(req.params.url)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
